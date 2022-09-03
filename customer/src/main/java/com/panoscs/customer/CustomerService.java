cc9aeea47595591a400ec5f84d8ddc5c790517ba
Panagiotis Tsiavos <panos20.pt@gmail.com>
2022-09-03 08:22:31 +0200

    Add Rabbit Amqp for async calls

--- customer/src/main/java/com/panoscs/customer/CustomerService.java
+++ customer/src/main/java/com/panoscs/customer/CustomerService.java
@@ -1,8 +1,8 @@
 package com.panoscs.customer;
 
+import com.panoscs.amqp.RabbitMQMessageProducer;
 import com.panoscs.clients.fraud.FraudCheckResponse;
 import com.panoscs.clients.fraud.FraudClient;
-import com.panoscs.clients.notification.NotificationClient;
 import com.panoscs.clients.notification.NotificationRequest;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -14,7 +14,7 @@ public class CustomerService {
 
     private final CustomerRepository customerRepository;
     private final FraudClient fraudClient;
-    private final NotificationClient notificationClient;
+    private final RabbitMQMessageProducer rabbitMQMessageProducer;
 
     public void registerCustomer(CustomerRegistrationRequest request) {
         Customer customer = Customer.builder()
@@ -31,13 +31,16 @@ public class CustomerService {
             throw new IllegalStateException("fraudster");
         }
 
-        // TODO: Make it async i.e add to queue
-        notificationClient.sendNotification(
-                new NotificationRequest(
-                        customer.getId(),
-                        customer.getEmail(),
-                        String.format("Hi %s, welcome!", customer.getFirstName())
-                )
+        NotificationRequest notificationRequest = new NotificationRequest(
+                customer.getId(),
+                customer.getEmail(),
+                String.format("Hi %s, welcome!", customer.getFirstName())
+        );
+
+        rabbitMQMessageProducer.publish(
+                notificationRequest,
+                "internal.exchange",
+                "internal.notification.routing-key"
         );
     }
 }
--- notification/src/main/java/com/panoscs/notification/NotificationApplication.java
+++ notification/src/main/java/com/panoscs/notification/NotificationApplication.java
@@ -5,7 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@SpringBootApplication
+@SpringBootApplication(
+        scanBasePackages = {
+                "com.panoscs.notification",
+                "com.panoscs.amqp"
+        }
+)
 @EnableEurekaClient
 @EnableFeignClients(
         basePackages = "com.panoscs.clients"
@@ -14,4 +19,11 @@ public class NotificationApplication {
     public static void main(String[] args) {
         SpringApplication.run(NotificationApplication.class, args);
     }
+
+//    @Bean
+//    CommandLineRunner commandLineRunner(RabbitMQMessageProducer producer, NotificationConfig notificationConfig) {
+//        return args -> {
+//            producer.publish("foo", notificationConfig.getInternalExchange(), notificationConfig.getInternalNotificationRoutingKey());
+//        };
+//    }
 }
